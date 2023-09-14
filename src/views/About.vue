5d9b7625da992b2787c72d217e71371ea59e260e
yangjieping <yuwenlinghao@gmail.com>
2023-09-14 18:46:22 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<script></script>
+<template>
+    <div>
+        About
+    </div>
+</template>
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<script></script>
+<template>
+    <div>
+        Home
+    </div>
+</template>
+<style></style>
